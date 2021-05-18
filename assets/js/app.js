2e1b66eb318f42a3e851cd4ca7801f94e6b0ce70
Miguel Fernandes <57355863+miguelfernandesidesign@users.noreply.github.com>
2021-05-18 21:46:33 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        message: message.value
+
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'sucess'){
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            message.value = '';
+        }else{
+            alert('Something went wrong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+
+
+})
